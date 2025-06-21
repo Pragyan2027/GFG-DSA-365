a43d8442812217d15980d9934e1bba8cd920b110
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-06-21 09:19:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int catchThieves(char[] arr, int k) {
+        // code here
+        
+        int thievesCaught = 0;
+    
+    Queue<Integer> thieves = new LinkedList<>();
+    
+    for(int i = 0 ; i< arr.length;i++){
+        if(arr[i] == 'T'){
+            thieves.add(i);
+        }
+    }
+    for(int i = 0 ; i < arr.length && !thieves.isEmpty();i++){
+         if (arr[i] == 'P') {
+                
+                while (!thieves.isEmpty()) {
+                    Integer thiefPosition = thieves.peek();
+                    
+                    if (thiefPosition > i + k) {
+                        break;
+                    }
+                    
+                    if (thiefPosition < i - k) {
+                        thieves.remove();
+                        continue;
+                    }
+                    
+                    // thief can be caught
+                    thieves.remove();
+                    thievesCaught++;
+                    break;
+                }
+                
+            }
+        }
+        
+        return thievesCaught;
+        
+    }
+}
